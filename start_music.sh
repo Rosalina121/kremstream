aae803d057729d17eea16ab0927b00df0bfab267
Alina Rosa <rosalina@palitechnika.com>
2024-06-26 15:04:19 +0200

    update for new cava-config

---
+++ start_music.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# change this to where you have checked out the Terminal-Music-Controller
+cd /home/rosalina/Gits/Terminal-Music-Controller/
+source .venv/bin/activate
+# -c creates cava-config for cava colors
+python3 main.py -c
